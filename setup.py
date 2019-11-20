e93c6a199efa50a77fb8a6846dde4e5f3e224b06
rldan <rldan@thoughtworks.com>
2019-11-20 15:22:28 +0800

    Bump version: 1.2.1 → 1.2.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = (HERE / "README.pypi.md").read_text()
 
 setup(
     name="crawling-gocd",
-    version="1.2.1",
+    version="1.2.2",
     description="crawling the gocd build histories of pipelines and calculate the metrics",
     long_description=README,
     long_description_content_type="text/markdown",
